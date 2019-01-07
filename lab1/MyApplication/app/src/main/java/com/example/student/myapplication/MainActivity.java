02ddf2aba87faed7bc0e10c9e7bb5827df04943d
krishna gupta <krishna05564@gmail.com>
2019-01-07 11:34:48 +0530

    lab 1 commit

---
+++ lab1/MyApplication/app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.student.myapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.view.Menu;
+import android.util.Log;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.EditText;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+    public void sendMessage(View view)
+    {
+        // Do something in response to button click
+        EditText name = (EditText)findViewById(R.id.name);
+        EditText phone = (EditText)findViewById(R.id.phone);
+        EditText email = (EditText)findViewById(R.id.email);
+        EditText password = (EditText)findViewById(R.id.password);
+
+        boolean email_b=(!TextUtils.isEmpty(email.toString())&& Patterns.EMAIL_ADDRESS.matcher(email.toString()).matches());
+        boolean name_b=(!TextUtils.isEmpty(name.toString()));
+        boolean password_b=(!TextUtils.isEmpty(password.toString()));
+        boolean phone_b= (phone.getText().length()==10);
+
+        if(name_b && password_b && !email_b){
+            Toast toast = Toast.makeText(getApplicationContext(),"Correct details entered",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        else{
+            Toast toast = Toast.makeText(getApplicationContext(),"wrong details",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
